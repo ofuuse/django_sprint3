e706ad0af939f5396e3ede95782c2dae2ccf8575
Dmitriy Durynichev <ofuuse@gmail.com>
2025-12-23 21:37:31 +0300

    recommended fix1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone  # Добавим импорт
 from core.query_sets import post_query, category_query
 
 
 def index(request):
     template = "blog/index.html"
-    post_list = post_query()[:5]  # БЕЗ .order_by("-pub_date")
+    post_list = post_query()[:5]
     context = {"post_list": post_list}
     return render(request, template, context)
 
@@ -25,6 +26,10 @@ def category_posts(request, category_slug):
         category_query(),
         slug=category_slug,
     )
-    post_list = post_query().filter(category__slug=category_slug)[:10]
+    post_list = category.cat_posts.filter(
+        is_published=True,
+        pub_date__lte=timezone.now(),
+        category__is_published=True,
+    )[:10]
     context = {"category": category, "post_list": post_list}
     return render(request, template, context)
